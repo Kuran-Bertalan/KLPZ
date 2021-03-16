0ef95f63d1c32348ec08279cb29fac67d9dada18
PQ653P <zsigulipuszz@gmail.com>
2021-03-16 12:18:23 +0100

    A "Code"-hoz a protected mappába hozzáadtam a nav.php fájlt.

---
+++ Code/protected/nav.php
@@ -0,0 +1,25 @@
+<hr>
+
+<a href="index.php">Home</a>
+<?php if(!IsUserLoggedIn()) : ?>
+	<span> &nbsp; | &nbsp; </span>
+	<a href="index.php?P=login">Login</a>
+	<span> &nbsp; | &nbsp; </span>
+	<a href="index.php?P=register">Register</a>
+<?php else : ?>
+	<span> &nbsp; | &nbsp; </span>
+	<a href="index.php?P=game">Game</a>
+	<?php if(isset($_SESSION['permission']) && $_SESSION['permission'] >= 1) : ?>
+		<span> &nbsp; || &nbsp; </span>
+		<a href="index.php?P=users">User list</a>
+		<a href="index.php?P=rules"> SZABÁLYOK</a>
+		<span> &nbsp; | &nbsp; </span>
+		<span> &nbsp; || &nbsp; </span>
+	<?php else : ?>
+		<span> &nbsp; | &nbsp; </span>
+	<?php endif; ?>
+
+	<a href="index.php?P=logout">Logout</a>
+<?php endif; ?>
+
+<hr>
