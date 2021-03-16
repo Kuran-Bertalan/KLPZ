58d625b8250644d88e7200ba3ff01950c2e8002c
Kurán Bertalan <kberci99@hotmail.hu>
2021-03-16 13:19:49 +0100

    game.js-ben hozzáadtam a deck class-t

--- Code/public/js/game.js
+++ Code/public/js/game.js
@@ -9,6 +9,26 @@ class Card {
     }
 }
 
+Card.SUITS = ['SPADES', 'HEARTS', 'DIAMONDS', 'CLUBS'];
+Card.VALUES = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
+
+class Deck {
+    constructor() {
+        this.cards = [];
+        this.fillDeck();
+    }
+
+    fillDeck() {
+        this.cards = [];
+        for (let i = 0; i < Card.SUITS.length; i++) {
+            for (let j = 0; j < Card.VALUES.length; j++) {
+                let card = new Card(Card.SUITS[i], Card.VALUES[j]);
+                this.cards.push(card);
+            }
+        }
+    }
+
+
 class Game {
     constructor(
         msgDisplay, moneyDisplay,
