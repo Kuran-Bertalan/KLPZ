bcbd6642de92fdfcb095c87b0ca88ace9480a46b
Csaba_Lehóczky <lcs000x@gmail.com>
2021-03-16 12:53:04 +0100

    Megírtam a BlackJack játékhoz tartozó játékszabályt és hozzáadtam a rules.php-hoz

---
+++ Code/protected/rules/rules.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset ="UTF-8"/>
+</head>
+	<title>Játékszabályok</title>
+	<link rel="stylesheet" type="text/css" href="<?php echo PUBLIC_DIR.'css/style.css'; ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<body>
+	<h1> Blackjack (huszonegy) játékszabály </h1>
+		<p class="szoveg">
+			A blackjack minden egyes körében az asztalnál ülő játékosok azzal kezdenek,
+			hogy betesznek egy tétet a saját megfelelő tétpozíciójukra, amit „doboz”-nak is
+			neveznek. Minden egyes asztal egyértelműen mutatja a játékos által rakható
+			minimális és maximális tétet. A tét nagyságát a zseton ikonra kattintva lehet
+			kiválasztani, ami kijelzi a kívánt tét helyes értékét. Miután minden tétet
+			feltettek, a játékosoknak két kártyát osztanak ki képpel felfelé a dobozukba,
+			és az osztó vagy egy kártyát kap (európai stílus), vagy két kártyát, egyiket képpel felfelé (atlantic city stílus), attól függően, milyen játék zajlik.
+		</p>
+	</center>
+</body>
+</html>
