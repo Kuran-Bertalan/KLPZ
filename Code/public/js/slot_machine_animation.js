5d2272776ccf763db5a2cf8f607ec7d95e2052bb
420DOMINICK <zsigulipuszz@gmail.com>
2021-04-22 19:17:13 +0200

    s_m_animation.js létrehoztam, és az animate function részt megírtam.

---
+++ Code/public/js/slot_machine_animation.js
@@ -0,0 +1,21 @@
+(function($) {
+	$._spritely = {
+		animate: function(options) {
+			var el = $(options.el);
+			var el_id = el.attr('id');
+			options = $.extend(options, $._spritely.instances[el_id] || {});
+			if (options.play_frames && !$._spritely.instances[el_id]['remaining_frames']) {
+				$._spritely.instances[el_id]['remaining_frames'] = options.play_frames + 1;
+			}
+			if (options.type == 'sprite' && options.fps) {
+				var frames;
+				var animate = function(el) {
+					var w = options.width, h = options.height;
+					if (!frames) {
+						frames = [];
+						total = 0
+						for (var i = 0; i < options.no_of_frames; i ++) {
+							frames[frames.length] = (0 - total);
+							total += w;
+						}
+					}
