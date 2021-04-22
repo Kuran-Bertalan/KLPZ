271974fa58bdf9988aa3684b4df37b68a36703f5
420DOMINICK <zsigulipuszz@gmail.com>
2021-04-22 19:52:33 +0200

    Start és a stop function-t megírtam, a slot machine-hez.

--- Code/public/js/slot.js
+++ Code/public/js/slot.js
@@ -45,3 +45,34 @@ $(document).ready(function() {
         $(el).spStop();
     }
 
+    Slot.prototype.start = function() {
+        var _this = this;
+        $(_this.el).addClass('motion');
+        $(_this.el).spStart();
+        _this.si = window.setInterval(function() {
+            if(_this.speed < _this.maxSpeed) {
+                _this.speed += _this.step;
+                $(_this.el).spSpeed(_this.speed);
+            }
+        }, 100);
+    };
+
+    Slot.prototype.stop = function() {
+        var _this = this,
+            limit = 30;
+        clearInterval(_this.si);
+        _this.si = window.setInterval(function() {
+            if(_this.speed > limit) {
+                _this.speed -= _this.step;
+                $(_this.el).spSpeed(_this.speed);
+            }
+            if(_this.speed <= limit) {
+                _this.finalPos(_this.el);
+                $(_this.el).spSpeed(0);
+                $(_this.el).spStop();
+                clearInterval(_this.si);
+                $(_this.el).removeClass('motion');
+                _this.speed = 0;
+            }
+        }, 100);
+    };
