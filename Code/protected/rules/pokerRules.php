1ad07848909b82a685ec96b7943116d409f227a4
Unknown <zsadanyi.rozsa@gmail.com>
2021-04-29 11:14:49 +0200

    A póker szabályhoz hozzáadtam a játék célja leírást

--- Code/protected/rules/pokerRules.php
+++ Code/protected/rules/pokerRules.php
@@ -17,9 +17,27 @@
 		</p><h4> Lapokról </h4>
 		<p class="szoveg">
 		Kártya: 52 lapos, francia, dzsókerek nélkül.
+		<br>
 		Játékosok száma: 2,3,4,5,6,7,8,9,10
+		<br>
 		A lapok rangsora:
+		<br>
 		Ász, király, dáma, bubi, 10, 9, 8, 7, 6, 5, 4, 3, 2.
+		<br>
+		52 lapos pakli:
+		<br>
+		Egy pakli francia kártyának (jokerek nélkül) 4 különböző színe van,
+		és minden szín 13 különböző értékű lapból áll, ez adja ki az 52 lapos paklit.
+		A 4 szín sorrendje: ♣ treff (clubs) ♦ káró (diamond) ♥ kőr (hearts) ♠ pikk (spades).
+		Minden szín egyenlő értékűnek számít a pókerkezek értékelésénél.
+		A 13 lap értékének sorrendje: A (ász), 2, 3, 4, 5, 6, 7, 8, 9, 10, J (bubi), Q (dáma), K (király)
+		Az ász nem csak az egyes számot jelöli, hanem a király után egy ász lap, vagyis a legmagasabb lapnak minősül.
+		
+		</p></p><h4> A játék célja: </h4>
+		<p class="szoveg">
+		Minél magasabb rangú lapkombinációk gyűjtése a kézben,illetve a licitálás során az ellenfelek elrettentése.
+		Megnyeri a partit az a személy, aki a licitálás befejezése után az aktív játékosok közül a legértékesebb kombinációt mutatja be.
+		Lapjainak bemutatása nélkül nyerhet az a játékos, aki ellenfél nélkül, egyedül marad a licitálásban.
 		</p>
 	</center>
 </body>
