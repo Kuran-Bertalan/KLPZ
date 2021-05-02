2ff7bbab2c547a628f094b1631726659705a5395
Csaba_Lehóczky <lcs000x@gmail.com>
2021-05-02 18:57:46 +0200

    Slot-Machine-hez tartozó előtörténet megírása

--- Code/protected/rules/slotMachineRules.php
+++ Code/protected/rules/slotMachineRules.php
@@ -7,5 +7,12 @@
 	<link rel="stylesheet" type="text/css" href="<?php echo PUBLIC_DIR.'css/style.css'; ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 <body>
+	<h1> Slot-Machine (Nyerögép) játékszabály </h1>
+        <h4> Előtörténete </h4>
+		<p class="szoveg">
+		A nyerőgépet a 19.század végén találták fel és kezdték alkalmazni a New yorki bárokban és kaszinókban. 
+        A gépet egy 5 centessel lehetett üzemeltetni, és mivel ez egy elhanyogolható összeg így a szegényebb rétegnek is 
+        megvolt a saját szerencsepróbája.
+		</p>
 </body>
 </html>
