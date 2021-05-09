67b02350333e79318d0687e2d0da7055f7aee6f3
Unknown <zsadanyi.rozsa@gmail.com>
2021-05-09 13:54:07 +0200

    Tesztekhez hozzáadtam a Deck-test-fill tesztet

---
+++ Code/public/Tesztek/Deck-fill-test/deck.js
@@ -0,0 +1,35 @@
+class Card {
+  constructor(suit, value) {
+    this.suit = suit;
+    this.value = value;
+    this.revealed = false;
+  }
+}
+
+Card.SUITS = ["SPADES", "HEARTS", "DIAMONDS", "CLUBS"];
+Card.VALUES = [
+  "A",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K",
+];
+
+function fillDeck() {
+  cards = [];
+  for (let i = 0; i < Card.SUITS.length; i++) {
+    for (let j = 0; j < Card.VALUES.length; j++) {
+      let card = new Card(Card.SUITS[i], Card.VALUES[j]);
+      cards.push(card);
+    }
+  }
+}
+module.exports = fillDeck;
---
+++ Code/public/Tesztek/Deck-fill-test/deck.test.js
@@ -0,0 +1,5 @@
+const deck = require("./deck");
+
+test("adds 1 + 2 to equal 3", () => {
+  expect(deck()).not.toBe(0);
+});
